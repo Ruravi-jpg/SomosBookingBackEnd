6a2791b58be370be2d411d56d1d77023fe4a7e81
Ruravi-JPG <joseruravi@hotmail.com>
2022-02-25 12:04:52 -0600

    initial commit

---
+++ src/main/java/com/somosbooking/somosBack/SomosBackApplication.java
@@ -0,0 +1,13 @@
+package com.somosbooking.somosBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomosBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/somosbooking/somosBack/SomosBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somosbooking.somosBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomosBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
