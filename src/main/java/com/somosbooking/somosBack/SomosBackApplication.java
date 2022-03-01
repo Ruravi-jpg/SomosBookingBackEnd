c751ba0217dce6b04ee190d7d82793f922708385
Ruravi-JPG <joseruravi@hotmail.com>
2022-03-01 12:29:45 -0600

    Revert "conexión a heroku"
    This reverts commit d269ca34b9bd25c479d3bb6c7b83984dbb74b740.

--- src/main/java/com/somosbooking/somosBack/SomosBackApplication.java
+++ src/main/java/com/somosbooking/somosBack/SomosBackApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SomosBackApplication {
 
 	public static void main(String[] args) {
