6936e21139a425a1db621fe73e410b333650fab2
Ruravi-JPG <joseruravi@hotmail.com>
2022-03-01 11:04:38 -0600

    config para heroku

--- src/main/java/com/somosbooking/somosBack/SomosBackApplication.java
+++ src/main/java/com/somosbooking/somosBack/SomosBackApplication.java
@@ -2,8 +2,10 @@ package com.somosbooking.somosBack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SomosBackApplication {
 
 	public static void main(String[] args) {
